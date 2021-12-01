34321d5c02921e093c41b20b5fd941a088638500
mjprogramation <anassboutaline@gmail.com>
2021-12-01 13:01:42 +0100

    Add rgb to hsl

--- src/FromRgb.js
+++ src/FromRgb.js
@@ -38,10 +38,80 @@ class FromRgb {
         return (this.singleToHex(this.red) + this.singleToHex(this.green) + this.singleToHex(this.blue)).toUpperCase()
     }
 
+    getMaxMinValue (value1, value2, value3) {
+
+        return {
+            min : Math.min(value1, value2, value3),
+            max : Math.max(value1, value2, value3)
+        }
+    }
+
+
+    calculateHue (max, delta, {red, blue, green}) {
+        if(delta == 0){
+            return 0
+        }
+
+        if(max == red) {
+            return ((green - blue) / delta) % 6
+        }
+
+        if(max == green) {
+            return (blue - red) / delta + 2
+        }
+
+        return (red- green) / delta + 4
+
+    }
+
+    convertToHsl () {
+        let h = 0,
+            s = 0,
+            l = 0
+
+
+        const red   = this.red/255,
+              green = this.green/255,
+              blue  = this.blue/255
+
+        const { min, max } = this.getMaxMinValue(red, green, blue)
+
+        const delta = max - min
+
+
+        // Hue
+        h = this.calculateHue(max, delta, {
+            red: red,
+            green: green,
+            blue: blue
+        })
+
+        h = Math.round(h * 60);
+    
+        
+        (h < 0) ? h = (h + 360).toFixed(1) : null
+
+        // Ligntness
+        l = (max + min) / 2
+        
+
+        // Saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
+        s = +(s * 100).toFixed(1)
+        l = +(l * 100).toFixed(1)
+
+        return {
+            h: h,
+            s: s,
+            l: l
+        }
+
+    }
+
 
 }
 
 
-const rgb = new FromRgb(0,10,255)
+const rgb = new FromRgb(0,1,0)
 
-console.log(rgb.convertToHex())
+console.log(rgb.convertToHsl())
