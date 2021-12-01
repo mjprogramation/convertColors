7c96a04462ea9622c2d33819212d06b87c116b7c
mjprogramation <anassboutaline@gmail.com>
2021-12-01 13:14:37 +0100

    create convert to hsv method

--- src/FromRgb.js
+++ src/FromRgb.js
@@ -114,7 +114,7 @@ class FromRgb {
      * } 
      */
     convertToHsl () {
-        
+
         let h = 0,
             s = 0,
             l = 0
@@ -157,10 +157,52 @@ class FromRgb {
 
     }
 
+    /**
+     * Convert Current Object To HSV
+     * 
+     * @returns {h, s, v}
+     */
+    convertToHsv () {
+        let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+        rabs = this.red / 255;
+        gabs = this.green / 255;
+        babs = this.blue / 255;
+        v = Math.max(rabs, gabs, babs),
+        diff = v - Math.min(rabs, gabs, babs);
+        diffc = c => (v - c) / 6 / diff + 1 / 2;
+        percentRoundFn = num => Math.round(num * 100) / 100;
+        if (diff == 0) {
+            h = s = 0;
+        } else {
+            s = diff / v;
+            rr = diffc(rabs);
+            gg = diffc(gabs);
+            bb = diffc(babs);
+    
+            if (rabs === v) {
+                h = bb - gg;
+            } else if (gabs === v) {
+                h = (1 / 3) + rr - bb;
+            } else if (babs === v) {
+                h = (2 / 3) + gg - rr;
+            }
+            if (h < 0) {
+                h += 1;
+            }else if (h > 1) {
+                h -= 1;
+            }
+        }
+        return {
+            h: Math.round(h * 360),
+            s: percentRoundFn(s * 100),
+            v: percentRoundFn(v * 100)
+        };
+    }
+
 
 }
 
 
 const rgb = new FromRgb(0,1,0)
 
-console.log(rgb.convertToHsl())
+console.log(rgb.convertToHsv())
