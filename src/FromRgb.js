5f6de0e7d6b5f33bbfde08b680982220150b51f8
mjprogramation <anassboutaline@gmail.com>
2021-12-02 10:33:52 +0100

    finishing Rgb to Cmyk method

--- src/FromRgb.js
+++ src/FromRgb.js
@@ -84,7 +84,7 @@ class FromRgb {
      * 
      * @param { integer } max 
      * @param { integer } delta 
-     * @param { Object } thirdParam 
+     * @param { Object } ColorsRatios 
      * 
      * @returns { integer }
      */
@@ -200,9 +200,41 @@ class FromRgb {
     }
 
 
+
+    convertToCmyk(){
+        var c = 1 - (this.red / 255);
+        var m = 1 - (this.green/ 255);
+        var y = 1 - (this.blue / 255);
+        var k = Math.min(c, Math.min(m, y));
+        
+        c = (c - k) / (1 - k);
+        m = (m - k) / (1 - k);
+        y = (y - k) / (1 - k);
+        
+        
+        c = Math.round(c * 10000) / 100;
+        m = Math.round(m * 10000) / 100;
+        y = Math.round(y * 10000) / 100;
+        k = Math.round(k * 10000) / 100;
+       
+        
+        c = isNaN(c) ? 0 : c;
+        m = isNaN(m) ? 0 : m;
+        y = isNaN(y) ? 0 : y;
+        k = isNaN(k) ? 0 : k;
+        
+        return {
+            c: Math.ceil(c),
+            m: Math.ceil(m),
+            y: Math.ceil(y),
+            k: Math.ceil(k)
+        }
+    }
+
+
 }
 
 
-const rgb = new FromRgb(0,1,0)
+const rgb = new FromRgb(255, 50, 13)
 
-console.log(rgb.convertToHsv())
+console.log(rgb.convertToCmyk())
